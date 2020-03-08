1a98911e68867a17354a81b3b5ee4916a76f8913
Ferius057 <60037873+Ferius057@users.noreply.github.com>
2020-03-08 18:54:49 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    private static Working TestNumber;
+    public static void main(String[] args) {
+        while (true) {
+            long num;
+            Scanner t = new Scanner(System.in);
+            System.out.print("Введите число от 1 до 999.999.999.999 [число с минусом писать {-}]: ");
+            num = (long) t.nextDouble();
+            System.out.println(TestNumber.WR(num));
+        }
+    }
+}
